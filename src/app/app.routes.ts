96a483c19e3381f52962789b43540eafcc3ce071
teaguejk <teaguejk@appstate.edu>
2024-02-22 15:35:17 -0500

    more components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { QuickstartComponent } from './components/quickstart/quickstart.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 export const routes: Routes = [
-    {'path':'home', component:HomeComponent}
+    {'path':'home', component:HomeComponent},
+    {'path':'start', component:QuickstartComponent},
+    {'path':'skills', component:SkillsComponent}
 ];
