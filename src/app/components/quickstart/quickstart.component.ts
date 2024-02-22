06c43c497be8efd59eaf26a15e93f6018ec58747
teaguejk <teaguejk@appstate.edu>
2024-02-22 14:31:09 -0500

    home page content

---
+++ src/app/components/quickstart/quickstart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quickstart',
+  standalone: true,
+  imports: [],
+  templateUrl: './quickstart.component.html',
+  styleUrl: './quickstart.component.css'
+})
+export class QuickstartComponent {
+
+}
